36991870ac89d43692259db082a0bf542b842c4c
flumoun <114039150+flumoun@users.noreply.github.com>
2025-12-08 15:54:58 +0800

    chore: replace python with python3 command

--- run.sh
+++ run.sh
@@ -17,14 +17,14 @@ run_results() {
     local language=$1
 
     log "[INFO] Running inference for language: ${language}"
-    python ./My_RAG/main.py \
+    python3 ./My_RAG/main.py \
         --query_path ./dragonball_dataset/queries_show/queries_${language}.jsonl \
         --docs_path ./dragonball_dataset/dragonball_docs.jsonl \
         --language ${language} \
         --output ./predictions/predictions_${language}.jsonl
 
     log "[INFO] Checking output format for language: ${language}"
-    python ./check_output_format.py \
+    python3 ./check_output_format.py \
         --query_file ./dragonball_dataset/queries_show/queries_${language}.jsonl \
         --processed_file ./predictions/predictions_${language}.jsonl
 
