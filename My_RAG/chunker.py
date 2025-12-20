0b56d9088cdcb734bb32721f62b2fa29ee626016
bear <kidflash7011@gmail.com>
2025-12-20 23:54:52 +0800

    submission: use contextual retrieval and domain router -> use cache, 4omini_en+zh chunk cache

--- My_RAG/chunker.py
+++ My_RAG/chunker.py
@@ -105,7 +105,7 @@ def recursive_chunk_documents(docs, language, chunk_size=1000, chunk_overlap=200
 
     # Build cache path
     if language == "zh":
-        cache_path = f"chunk_cache/{language}_contextual_chunksize{chunk_size}.json"
+        cache_path = f"chunk_cache/{language}_4omini_contextual_chunksize{chunk_size}.json"
     else:
         cache_path = f"chunk_cache/{language}_4omini_contextual_chunksize{chunk_size}.json"
 
